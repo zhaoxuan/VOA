b92118fe3c5277607d38d7f47bc86db42b278814
zhaoxuan <zhaoxuan1727@gmail.com>
2012-11-03 15:36:47 +0800

    log

--- lib/efile.rb
+++ lib/efile.rb
@@ -9,7 +9,7 @@ class Efile
     @download_url = 'http://down.51voa.com/201210/se-ed-mali-education-web-24oct12.mp3'
   end
 
-  def download
+  def download(url)
     filename = @download_url[@download_url.rindex('/')+1, @download_url.length-1]
 
     download_file = File.new("./download_file/" + filename, 'w+')
@@ -18,6 +18,16 @@ class Efile
     download_file.flush
     download_file.close
  
+  end
+
+  def self.download(url)
+    filename = url[url.rindex('/')+1, url.length-1]
+
+    download_file = File.new("./download_file/" + filename, 'w+')
+    download_file.binmode
+    download_file << open(url).read
+    download_file.flush
+    download_file.close
   end
   
 end
--- lib/ehtml.rb
+++ lib/ehtml.rb
@@ -1,20 +1,42 @@
 #encoding: utf-8
 require 'rubygems'
 require 'debugger'
+require 'mechanize'
+require File.expand_path("../efile", __FILE__)
 
 class Ehtml
-  
-  def html_search
-    
+
+  def initialize
+    @agent                  = Mechanize.new
+    @agent.user_agent_alias = 'Mac Safari'
+  end
+
+  def get_page(page_url)
+    @agent.get(page_url)   
+  end
+
+  def get_article_array(page)
+    result = []
+
+    page.search("//div[@id='rightContainer']/span[@id='list']/ul").children.each do |v|
+        result << v.children.last.attribute('href').content
+    end
+    result
+  end
+
+  def get_download_url(page)
+    file_url = page.search("//div[@id='menubar']").children[1].attribute('href').content
+  end
+
+  def download_english(url)
+    Efile.download(url)
+  end
+
+  def get_download_caption_url(page)
     
   end
 
-  def get_home_page
-    @home_page_html = Nokogiri::HTML(open('http://www.51voa.com/'))
-    @home_page_html.xpath('//span[@id="list"]/ul').each do |v|
-      
-    end  
-  end
+ 
 
 
 
