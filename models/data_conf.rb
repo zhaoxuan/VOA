197ce8382409ab60f61eb5323c94b8e992816d95
zhaoxuan <zhaoxuan1727@gmail.com>
2013-08-29 09:06:46 +0800

    delete ruby-debug

--- models/data_conf.rb
+++ models/data_conf.rb
@@ -1,11 +1,10 @@
 #encoding: utf-8
 require 'rubygems'
 require 'active_record'
-require 'ruby-debug'
 
 ActiveRecord::Base.establish_connection(  
-  :adapter => "mysql",  
-  :host => "localhost",  
+  :adapter => "mysql",
+  :host => "localhost",
   :database => "voa",
   :username => "root",
   :password => "123456",
