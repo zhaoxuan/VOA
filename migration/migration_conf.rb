efe0d6d4fb12c383b27ff90e4a261418746ea5ca
zhaoxuan <zhaoxuan1727@gmail.com>
2013-10-26 16:23:50 +0800

    delete some unnecessary code

--- migration/migration_conf.rb
+++ migration/migration_conf.rb
@@ -2,25 +2,11 @@
 require 'rubygems'
 require 'active_record'
 
-ActiveRecord::Base.establish_connection(  
-  :adapter => "mysql",  
-  :host => "localhost",  
+ActiveRecord::Base.establish_connection(
+  :adapter => "mysql",
+  :host => "localhost",
   :database => "voa",
   :username => "root",
   :password => "123456",
   :encoding => 'utf8'
 )
-module MigrationConf
-  module ClassMethods
-    
-  end
-  
-  module InstanceMethods
-    
-  end
-  
-  def self.included(receiver)
-    receiver.extend         ClassMethods
-    receiver.send :include, InstanceMethods
-  end
-end
